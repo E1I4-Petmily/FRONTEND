b2a6468354a0e803542866d0a85608e3d6590351
Jaeyeon-23 <reenact115@gmail.com>
2025-12-10 15:41:32 +0900

    [Feat] 사용자 마이페이지 로그아웃 추가

--- src/pages/my-page.tsx
+++ src/pages/my-page.tsx
@@ -110,6 +110,18 @@ export default function MyPage() {
         ))}
         <PetCard type="add" onAddClick={() => navigate("/register/welcome")} />
       </div>
+
+      <div className="w-full flex justify-center mt-10 mb-3">
+        <button
+          onClick={() => {
+            localStorage.removeItem("accessToken");
+            navigate("/");
+          }}
+          className="text-red-500 text-sm font-medium"
+        >
+          로그아웃
+        </button>
+      </div>
     </div>
   );
 }
